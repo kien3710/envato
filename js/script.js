99300263c6e21c11ef54ce57f6a12e9e628400fe
kien <kien3710@gmail.com>
2023-02-09 10:40:39 +0700

    push

--- js/script.js
+++ js/script.js
@@ -8,16 +8,20 @@ function bars() {
 }
 function sidebar() {
   var sidebars = document.querySelector(".sidebar-tab");
+  var sidebarFooter = document.querySelector(".sidebar__footer");
   sidebars.classList.toggle("sidebar-tab-active");
   console.log(":a");
+  sidebarFooter.classList.toggle("display-none");
 }
 function search() {
   var logo = document.querySelector(".logo");
   var search = document.querySelector(".search-table");
   var iconSearch = document.querySelector(".fa-magnifying-glass");
+  var iconSearch2 = document.querySelector(".icon-search");
   logo.classList.toggle("search-active");
   search.classList.toggle("search-tab-active");
   iconSearch.classList.toggle("search-active");
+  iconSearch2.classList.toggle("display-none");
 }
 function toggle() {
   var sidebars = document.querySelector(".sidebar-shadow");
@@ -33,6 +37,4 @@ function toggle() {
   sidebarFooter.classList.toggle("display-none");
   filtes.classList.toggle("display-none");
   filtess.classList.toggle("display-none");
-
-  console.log("🚀  ~ file: script.js:33 ~ toggle ~ sidebar_footer", filtess);
 }
